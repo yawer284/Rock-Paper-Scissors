cbfd10700f8ee2c5c4008a240a54add63a43e1cc
yawer284 <abdallahalyawar2004@gmail.com>
2026-06-05 15:42:23 +0300

    editing a few things in the JS code
    1- putting the variable randomNumber inside the random function to prevent any future mistakes
    2- use === instead of == and convert the prompt result to numbers by Number()

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let randomNumber = Math.floor(Math.random() * 3) + 1;
 let humanScore, computerScore = 0;
 
 function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    
     if (randomNumber === 1) {
         console.log("Rock");
     } else if (randomNumber === 2) {
@@ -12,17 +13,15 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Choose a random number between (1 - 3)? (1 == Rock, 2 == Paper, and 3 == Scissors)");
+    let choice = Number(prompt("Choose a random number between (1 - 3)? (1 == Rock, 2 == Paper, and 3 == Scissors)"));
 
-    if (choice == 1) {
+    if (choice === 1) {
         alert("Rock");
-    } else if (choice == 2) {
+    } else if (choice === 2) {
         alert("Paper");
     } else {
         alert("Scissors");
     }
 }
 
-function playRound(getComputerChoice, getHumanChoice) {
-    
-}
+getComputerChoice()

0313a58f9580391a51825cb372a4c2983534b8b4
yawer284 <abdallahalyawar2004@gmail.com>
2026-06-06 14:44:07 +0300

    adding function that compare between the 2 players results for 1 round
    i create a function using variables and if else statements and increment and console.log() to print the result of the 2 players
    and assign to them a variables that linked to there functions.

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     
     if (randomNumber === 1) {
-        console.log("Rock");
+        return "rock";
     } else if (randomNumber === 2) {
-        console.log("Paper");
+        return "paper";
     } else {
-        console.log("Scissors");
+        return "scissors";
     }
 }
 
@@ -16,12 +16,43 @@ function getHumanChoice() {
     let choice = Number(prompt("Choose a random number between (1 - 3)? (1 == Rock, 2 == Paper, and 3 == Scissors)"));
 
     if (choice === 1) {
-        alert("Rock");
+        return "Rock";
     } else if (choice === 2) {
-        alert("Paper");
+        return "Paper";
     } else {
-        alert("Scissors");
+        return "Scissors";
     }
 }
 
-getComputerChoice()
+function playRound(humanChoice, computerChoice) {
+    let input = humanChoice.toLowerCase();
+
+    if(input === computerChoice) {
+        console.log("it's a draw");
+    } else if(input === "rock" && computerChoice === "paper") {
+        console.log("Computer win!");
+        return ++computerScore;
+    } else if(input === "paper" && computerChoice === "scissors") {
+        console.log("Computer win!");
+        return ++computerScore;
+    } else if(input === "scissors" && computerChoice === "rock") {
+        console.log("Computer Win!");
+        return ++computerScore;
+    } else if(computerChoice === "rock" && input === "paper") {
+        console.log("You Win!");
+        return ++humanScore;
+    } else if(computerChoice === "paper" && input === "scissors") {
+        console.log("You Win!");
+        return ++humanScore;
+    } else if(computerChoice === "scissors" && input === "rock") {
+        console.log("You Win!");
+        return ++humanScore;
+    } else {
+        console.log("nothing to say");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
